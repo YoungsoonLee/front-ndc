ed5cb7616dab3bd5c98bea03af2b56b6280fd951
JayJay <youngtip@gmail.com>
2018-12-10 16:20:42 +0900

    .

--- src/lib/api/billing.js
+++ src/lib/api/billing.js
@@ -6,4 +6,5 @@ export const getChargeItems = () => axios.get('/v1/billing/getChargeItems');
 export const getPaymentToken = ({token, itemid}) => axios.post('/v1/billing/getPaymentToken', {itemid}, {headers: {Authorization: "Bearer "+token}});
 
 export const getHash = ({token, service_id, external_id, item_id, item_name, item_amount}) => axios.post('/v1/billing/getDeductHash', { service_id, external_id, item_id, item_name, item_amount}, {headers: {Authorization: "Bearer "+token}});
+
 export const testBuyItem = ({token, service_id, external_id, item_id, item_name, item_amount, hash}) => axios.post('/v1/billing/buyItem', { external_id, service_id, item_id, item_name, item_amount, hash}, {headers: {Authorization: "Bearer "+token}});
